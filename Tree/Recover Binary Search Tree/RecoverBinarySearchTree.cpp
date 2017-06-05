b636ca0146e156e2a564d14b0b5261775daf384f
zhuoshen617 <zhuoshen617@gmail.com>
2017-06-04 20:16:17 -0400

    add two Q

---
+++ Tree/Recover Binary Search Tree/RecoverBinarySearchTree.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/*
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+*/
+
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+    }
+};
---
+++ Tree/Recover Binary Search Tree/Solution.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/*
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    void helper(TreeNode* root, TreeNode** first, TreeNode** second, TreeNode** prev)
+    {
+        if (!root)
+            return;
+        
+        helper(root->left, first, second, prev);
+     
+        if (!(*prev))
+            *prev = root;
+        else
+        {
+            if ((*prev)->val > root->val)
+            {
+                if (!(*first))
+                {
+                    *first = *prev;
+                    *second = root;
+                }
+                else
+                {
+                    *second = root;
+                }
+            }
+            
+            *prev = root;    
+        }
+        
+        helper(root->right, first, second, prev);
+    }
+
+    void recoverTree(TreeNode* root) 
+    {
+        TreeNode * first = NULL;
+        TreeNode * second = NULL;
+        TreeNode * prev = NULL;
+        helper(root, &first, &second, &prev);
+        swap(first->val, second->val);
+    }
+};
